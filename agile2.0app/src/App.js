54e6c0e511f0959901cca64f06f74dcacd9390f4
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-05-17 10:18:42 +0200

    Added router and Layout.js file

--- agile2.0app/src/App.js
+++ agile2.0app/src/App.js
@@ -1,27 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
-import FontTest from './components/testing/FontTest';
-import Shop from './views/Shop';
-import Footer from './views/Footer';
-import Homepage from './views/Homepage';
-import Navbar from './views/Navbar/Navbar';
-import Cart from './views/Cart';
-import Contactus from './views/Contactus';
-import FrontpageProductcard from './components/FrontpageProductcard';
-import Button from './components/Button/Button'
+import Layout from './views/Layout'
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <FontTest />
-      <Homepage/>
-      <Contactus/>
-      <FrontpageProductcard/>
-      <Button label='Hello this is a button' />
-      <Cart/>
-      <Shop/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

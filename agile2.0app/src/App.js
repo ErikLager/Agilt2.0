ff8d517d0dff08b6a0bfb456db5bdaacc75331d0
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-05-17 14:45:18 +0200

    Add shop and homepage to routing in app.js

--- agile2.0app/src/App.js
+++ agile2.0app/src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Layout from './views/Layout'
+import Homepage from './views/Homepage/Homepage'
+import Shop from './views/Shop'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          
+          <Route index element={<Homepage />} />
+          <Route path='sneakers' element={<Shop />} />
         </Route>
       </Routes>
     </BrowserRouter>

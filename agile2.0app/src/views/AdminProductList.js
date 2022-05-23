c89826ee7e8ef8abbfe1b4d85d2a4b50dbb769d8
Erik <43432787+ErikLager@users.noreply.github.com>
2022-05-23 16:03:39 +0200

    started on product list for admin

--- agile2.0app/src/views/AdminProductList.js
+++ agile2.0app/src/views/AdminProductList.js
@@ -1,7 +1,30 @@
 import productlist from "./mockProduct";    
 
 const AdminProductList = () => {
-    console.log(productlist);
+    return(
+        <>
+            <h1>List of all the products</h1>
+            {
+                productlist.map((item, index) => {
+                    console.log(item);
+
+                    return(
+                        <div className="productelement">
+                            <p>{item.name}</p>
+                            {/* <img src={item.pictures[0]}></img> */}
+                            <ul>
+                                {item.categories.map((item, index)=>{
+                                    return(
+                                        <li key={index}>{item}</li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
 };
 
 export default AdminProductList;

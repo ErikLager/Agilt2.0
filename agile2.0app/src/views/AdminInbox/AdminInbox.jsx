71fa8d357220a24e13c102687b6f9fc84b9b5e17
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-05-30 18:42:55 +0200

    Create function to fetch customer messages

--- agile2.0app/src/views/AdminInbox/AdminInbox.jsx
+++ agile2.0app/src/views/AdminInbox/AdminInbox.jsx
@@ -1,6 +1,19 @@
 import styles from './AdminInbox.module.css'
+import { useEffect } from 'react'
 
 const AdminInbox = () => {
+	
+	async function fetchCustomerMessages() {
+		console.log(process.env.REACT_APP_PORT)
+		const res = await fetch(`http://localhost:${process.env.REACT_APP_PORT}/api/getcustomermessages`).catch((error) => console.log(error))
+		const data = await res.json()
+		console.log(data)
+	}
+	
+	useEffect(() => {
+		fetchCustomerMessages()
+	}, [])
+	
 	return (
 		<p>I am the AdminInbox</p>
 	)

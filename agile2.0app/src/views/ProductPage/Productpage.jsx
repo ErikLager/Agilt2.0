d3458ac668de466e834c54c1f07b21ad602575ae
Geoblin <Oscar_niklasson94@hotmail.com>
2022-05-30 15:14:15 +0200

    I dont even

---
+++ agile2.0app/src/views/ProductPage/Productpage.jsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom'
+import styles from './Productpage.module.css'
+import React, { useState, Component } from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Product from '../../../../cool-fashion-database/api/products';
+
+const Products = require("Products");
+console.log(Products.generate());
+
+
+
+//Lägg till en carousel bild med en <div> med en Img tag i
+const Productpage = () => {
+
+	return (
+		<div className={styles.PPContainer}>
+            <Carousel className={styles.PPCarousel}>
+                <div>
+                <img className={styles.PPImg} src='https://ginatricotpc.imgix.net/pim/product-images/861605104/86160510405.jpg?auto=format,compress'/>
+                </div>
+                <div>
+                <img className={styles.PPImg} src="https://ginatricotpc.imgix.net/pim/product-images/861605104/86160510405.jpg?auto=format,compress"/>
+                </div>
+            </Carousel>
+          <div className={styles.PPRight}>
+          <h2>Productname</h2>
+        <br />
+        <br />
+        <p>{Product.desciption}</p>
+            <p>Price: Price goes here</p>
+            <p>Stock: {Product.inStock}</p>
+            <a className={styles.PPButton} href="">
+                Buy Now
+            </a>
+        </div>
+    </div>
+	)
+}
+
+export default Productpage;

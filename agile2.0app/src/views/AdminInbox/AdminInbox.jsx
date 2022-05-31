9300c498092499724f2a26f1f8692432798c1f84
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-05-31 21:27:24 +0200

    Show messages in Admin Inbox

--- agile2.0app/src/views/AdminInbox/AdminInbox.jsx
+++ agile2.0app/src/views/AdminInbox/AdminInbox.jsx
@@ -1,21 +1,48 @@
 import styles from './AdminInbox.module.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const AdminInbox = () => {
+	const [customerMessages, setCustomerMessages] = useState(null)
 	
 	async function fetchCustomerMessages() {
 		console.log(process.env.REACT_APP_PORT)
 		const res = await fetch(`http://localhost:${process.env.REACT_APP_PORT}/api/getcustomermessages`).catch((error) => console.log(error))
 		const data = await res.json()
-		console.log(data)
+		setCustomerMessages(data.customerMessages)
 	}
+	
+	useEffect(() => {
+		if (customerMessages) {
+			console.log(customerMessages)
+		}
+	})
 	
 	useEffect(() => {
 		fetchCustomerMessages()
 	}, [])
 	
 	return (
-		<p>I am the AdminInbox</p>
+		<>
+		<div className={styles.container}>
+			<h2>Admin inbox</h2>
+			<ul className={styles.inboxList}>
+				{customerMessages && (
+					customerMessages.map(message => {
+						return (
+							<li className={styles.inboxListItem}>
+								<h3 className={styles.inboxName}>{message.name}</h3>
+								<p>{message.email}</p>
+								<p>{message.message}</p>
+							</li>
+						)
+					})
+				)}
+			</ul>
+			{!customerMessages && (
+				<p>Loading...</p>			
+			)}
+			</div>
+		</>
 	)
 }
 

a647c621c31b31f366424a8557f762999543e3a7
Geoblin <Oscar_niklasson94@hotmail.com>
2022-06-02 14:54:58 +0200

    asd

--- agile2.0app/src/views/Shop.js
+++ agile2.0app/src/views/Shop.js
@@ -1,38 +1,35 @@
 import ProductCard from "../components/ProductCard";
 
+import { useEffect, useState } from "react";
+
+
+const url = 'http://localhost:5002/api/getproducts';
+
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+
+useEffect(() => {
+    async function fetchAllProducts() {
+        try {
+            let response = await fetch(url);
+            let data = await response.json();
+            setProducts(data.products);
+            console.log("Products",products);
+        } catch (error) {
+            console.log("Something wrong with fetch");
+        }
+    }
+    fetchAllProducts();
+},[]);
 
     return (
         <>
-            <div className="content">
-                <h1 className="center">This is the Shop</h1>
-                <div className="prodlist">
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                </div>
-            </div>
+            <ProductCard>
+                {products.map((product) => (
+                    <p key={product.name} product={product.name}></p>
+                ))}
+            </ProductCard>
         </>
     )
 };

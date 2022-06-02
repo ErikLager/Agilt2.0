ec8aea2cd888387ba89a5c539ef21573b9c087ba
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-06-02 11:51:26 +0200

    Create AdminCreateCategory and routing

---
+++ agile2.0app/src/views/AdminCreateCategory/AdminCreateCategory.jsx
@@ -0,0 +1,15 @@
+import styles from './AdminCreateCategory.module.css'
+import { useEffect, useState } from 'react'
+
+const AdminCreateCategory = () => {
+	
+	return (
+		<>
+			<div className={styles.container}>
+				<h2>This is AdminCreateCategory</h2>
+			</div>
+		</>
+	)
+}
+
+export default AdminCreateCategory

ccc4ecf6a8730db4980db46a17c79527725b83ad
Geoblin <Oscar_niklasson94@hotmail.com>
2022-06-20 17:23:28 +0200

    qwert

---
+++ cool-fashion-database/services/emailService.js
@@ -0,0 +1,25 @@
+const sgMail = require("@sendgrid/mail");
+
+require("dotenv").config();
+
+const orderConfirmed = (orderInfo) => {
+    sgMail.setApiKey(process.env.SENDGRID_KEY);
+    const msg = {
+        to:`${orderInfo.email}`,
+        from: "oscar.niklasson941@gmail.com",
+        subject: "Order confirmed",
+        text: "Your order has been confirmed and is being packed",
+        html: `<h1 style="color: red; text-transform: uppercase"> Howdy ${orderInfo.firstname}, your order has been confirmed!</h1>
+        <p>Thank you for shopping with us, we hope you will return soon!</p>`,
+    };
+    sgMail
+    .send(msg)
+    .then(() => {
+        console.log("Email sent");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+module.exports = { orderConfirmed };
